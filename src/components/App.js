2d017ba657040142968bf5c98d51fb118482f931
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-10-21 13:14:05 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
 
-const ItemDetail = () => {
-    const{id} = useParams()
+import React from "react";
+import './../styles/App.css';
+import {BrowserRouter, Routes, Route}  from  "react-router-dom"
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+
+const App = () => {
   return (
+    <BrowserRouter>
     <div>
-        <h1>Item {id}</h1>
-        <p>Description for Item {id}</p>
+      <Routes>
+        <Route path="/" element={<ItemList />}/>
+        <Route path="/items/:id" element={<ItemDetail />}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
-export default ItemDetail
+export default App
